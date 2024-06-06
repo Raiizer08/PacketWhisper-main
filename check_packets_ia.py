49ac9e88bbee3559f41abd29b2f76a755645c1f7
Raiizer <132910758+Raiizer08@users.noreply.github.com>
2024-06-06 22:34:05 +0200

    Update check_packets_ia.py

--- check_packets_ia.py
+++ check_packets_ia.py
@@ -1,32 +1,64 @@
-import pandas as pd
-from scapy.all import sniff, IP
-import argparse
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QMenuBar, QAction
 
-# initialise une liste vide packet_data qui sera utilisée pour stocker les informations sur chaque paquet capturé
-packet_data = []
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-def packet_callback(packet):
-    if IP in packet and packet[IP].src == '192.168.195.118':
-        packet_info = {
-            'src_ip': packet[IP].src,  # IP source du paquet
-            'dst_ip': packet[IP].dst,  # IP de destination du paquet
-            'src_port': packet.sport if packet.haslayer('TCP') or packet.haslayer('UDP') else None,  # port source du paquet
-            'dst_port': packet.dport if packet.haslayer('TCP') or packet.haslayer('UDP') else None,  # port de destination du paquet
-            'protocol': packet[IP].protocol,  # protocole du paquet
-            'length': len(packet)  # longueur totale du paquet
-        }
-        packet_data.append(packet_info)  # ajoute le dictionnaire packet_info à la liste packet_data
+    def initUI(self):
+        self.setWindowTitle("PacketWhisper")
+        self.setGeometry(100, 100, 800, 600)
 
-def main():
-    parser = argparse.ArgumentParser(description="Capture network packets and save them to a JSON file.")
-    parser.add_argument('-c', '--count', type=int, default=100, help='Number of packets to capture')
-    parser.add_argument('-f', '--file', type=str, default='packets.json', help='Output file name')
-    args = parser.parse_args()
-    
-    # capture le nombre de paquets demandés
-    sniff(prn=packet_callback, count=args.count)
-    # convertir en dataframe et sauvegarder en json
-    df = pd.DataFrame(packet_data)
-    df.to_csv(args.file, index=False)
-    print(f"Paquets capturés et sauvegardés dans '{args.file}'")
-    
+        # barre de menu
+        menuBar = QMenuBar(self)
+        fileMenu = menuBar.addMenu('File')
+        settingsMenu = menuBar.addMenu('Settings')
+        githubMenu = menuBar.addMenu('Github')
+
+        # ajout de deux actions dans le menu file
+        action1 = QAction('Open Workspace From File', self)
+        action1.triggered.connect(self.on_action_triggered)
+        fileMenu.addAction(action1)
+
+        action2 = QAction('Open Folder', self)
+        action2.triggered.connect(self.on_action_triggered)
+        fileMenu.addAction(action2)
+
+        # ajout de deux actions dans le menu settings
+        action1 = QAction('Appareance',self)
+        action1.triggered.connect(self.on_action_triggered)
+        settingsMenu.addAction(action1)
+
+        action2 = QAction('Output Format',self)
+        action2.triggered.connect(self.on_action_triggered)
+        settingsMenu.addAction(action2)
+
+        # ajout d'une action dans le menu github
+        action1 = QAction('My GitHub',self)
+        action1.triggered.connect(self.on_action_triggered)
+        githubMenu.addAction(action1)        
+        # label pour afficher le résultat
+        self.label = QLabel('Résultat de la capture : ', self)
+
+        # bouton pour démarrer le sniffer
+        button = QPushButton('Démarrer le sniffer', self)
+        button.clicked.connect(self.on_button_click)
+
+        layout = QVBoxLayout()
+        layout.addWidget(menuBar)
+        layout.addWidget(self.label)
+        layout.addWidget(button)
+        self.setLayout(layout)
+
+    def on_button_click(self):
+        self.label.setText("Sniffer Started")
+
+    def on_action_triggered(self):
+        self.label.setText("Action triggered !")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myApp = MyApp()
+    myApp.show()
+    sys.exit(app.exec_())
